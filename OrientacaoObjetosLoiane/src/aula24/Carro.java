fed15e780190948843eb34a5bb963329951c1ec2
j.filho <jfilhocobain13@hotmail.com>
2023-03-15 18:21:42 -0300

    aula 24 adicionada

---
+++ OrientacaoObjetosLoiane/src/aula24/Carro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package aula24;
+
+
+public class Carro {
+    
+    String marca;
+	String modelo;
+	int numPassageiros;
+	double capCombustivel;
+	double consumoCombustivel;
+}
---
+++ OrientacaoObjetosLoiane/src/aula24/TesteCarro.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package aula24;
+
+
+public class TesteCarro {
+
+    
+	public static void main(String[] args) {
+		
+		Carro van = new Carro();
+		van.marca = "Fiat";
+		van.modelo = "Ducato";
+		van.numPassageiros = 10;
+		van.capCombustivel = 100;
+		van.consumoCombustivel = 0.2;
+		
+		System.out.println(van.marca);
+		System.out.println(van.modelo);
+		
+		Carro fusca = new Carro();
+		fusca.marca = "Volkswagen";
+		fusca.modelo = "Fusca";
+		fusca.numPassageiros = 4;
+		fusca.capCombustivel = 30;
+		fusca.consumoCombustivel = 0.15;
+		
+		System.out.println(fusca.marca);
+		System.out.println(fusca.modelo);
+	}
+
+}
